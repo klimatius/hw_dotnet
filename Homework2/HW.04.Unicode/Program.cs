08e2e64d6fdd5e8353e5f0a795bf324a51ee2bd2
klimatius <klimatius@tut.by>
2021-10-09 23:05:49 +0300

    Homework2
    Homework2

---
+++ Homework2/HW.04.Unicode/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace HW._04.Unicode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Hello guys from the most popular programming course - C#!";
+            string text = default;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == ' ')
+                {
+                    Console.WriteLine();
+                    text += "\r\n";
+                }
+                else
+                {
+                    Console.WriteLine(str[i] + " = " + Convert.ToString(str[i], 10) + " = " + Convert.ToString(str[i], 2));
+                    text += str[i] + " = " + Convert.ToString(str[i], 10) + " = " + Convert.ToString(str[i], 2) + "\r\n"; ;
+                }
+            }
+            File.WriteAllText(@"C:\Users\klima\Desktop\HW4.txt", text);
+        }
+    }
+}
